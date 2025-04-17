33e872090db05754299074ad84444ecba0fe0c31
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-18 04:33:19 +0530

    Services Intercommunication using the feign client

--- 01_ServiceRegistry_Eureka_Client_2/src/main/java/com/pavan/client/Client1Communicator.java
+++ 01_ServiceRegistry_Eureka_Client_2/src/main/java/com/pavan/client/Client1Communicator.java
@@ -2,10 +2,11 @@ package com.pavan.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "01-ServiceRegistry-Eureka-Client-1")
 public interface Client1Communicator  {
 
 	@GetMapping(value="/responde/{msg}")
-	public String getReplayFromClient1(String msg);
+	public String getReplayFromClient1(@PathVariable String msg);
 }
