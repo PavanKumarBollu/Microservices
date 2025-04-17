99ae1d9a496c165aa1eb0d6bff793ca932248e54
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-18 03:51:11 +0530

    Services Intercommunication using the feign client

--- 01_ServiceRegistry_Eureka_Client_1/src/main/java/com/pavan/restcontroller/Responde.java
+++ 01_ServiceRegistry_Eureka_Client_1/src/main/java/com/pavan/restcontroller/Responde.java
@@ -1,8 +1,16 @@
 package com.pavan.restcontroller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Responde {
 
+	@GetMapping(value="/responde/{msg}")
+	public String responseToTheCall( @PathVariable String msg)
+	{
+		return "Hi I got Your Massage Don't worry I Am Here to Help You Out..*";
+	}
+	
 }
