01e537cc78ff35a50faaecdc3bea6e98ee6568c1
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-18 04:15:15 +0530

    Services Intercommunication using the feign client

--- 01_ServiceRegistry_Eureka_Client_2/src/main/java/com/pavan/restcontroller/Client2Controller.java
+++ 01_ServiceRegistry_Eureka_Client_2/src/main/java/com/pavan/restcontroller/Client2Controller.java
@@ -1,12 +1,14 @@
 package com.pavan.restcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pavan.client.Client1Communicator;
 
 @RestController
 public class Client2Controller {
+	
 	@Autowired
 	private Client1Communicator c;
 	
@@ -16,7 +18,9 @@ public class Client2Controller {
 	{
 		System.out.println("implementation class name is  :: " + c.getClass().getName());
 		
-		return 
+		String client2Msg = "Hi I Am Client 2 Is There anybody there to recieve my Massage....";
+		String replayFromClient1 = c.getReplayFromClient1(client2Msg);
+		return "client-2: " + "\n"+ client2Msg + "\nclient-1: \n" + replayFromClient1;
 	}
 	
 
