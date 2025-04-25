3f1e1e7aff4a605f01e505e9783baecf88840e9f
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-25 07:59:05 +0530

    Config Server

--- 02-Config-Server-Client/src/main/java/com/pavan/restController/MsgController.java
+++ 02-Config-Server-Client/src/main/java/com/pavan/restController/MsgController.java
@@ -1,6 +1,10 @@
 package com.pavan.restController;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
