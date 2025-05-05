413179c2af842e7e81f866d5853ea65967f0a8ac
PavanKumarB <bollupavankumar12@gmail.com>
2025-05-06 01:41:12 +0530

    Async Job Scheduler Task by LogiScope Company

--- AsynchronousJobScheduler_By_Pavan/src/main/java/com/pavan/service/JobExecutorService.java
+++ AsynchronousJobScheduler_By_Pavan/src/main/java/com/pavan/service/JobExecutorService.java
@@ -19,6 +19,7 @@ public class JobExecutorService {
 		{
 			case EMAIL -> {jobService.executeEmailTask(jobRequest.getParams());}
 			case REPORT -> {jobService.executeReportTask(jobRequest.getParams());}
+			case DB -> {jobService.executeDbConnection(jobRequest.getParams());}
 			default -> {throw new IllegalArgumentException("UnSupported Task Type");}
 		}
 	}
