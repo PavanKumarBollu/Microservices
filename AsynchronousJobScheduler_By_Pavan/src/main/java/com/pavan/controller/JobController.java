77896e1d8313efe35c3851fbaad9dbbcc6cb01ec
PavanKumarB <bollupavankumar12@gmail.com>
2025-05-06 00:06:33 +0530

    Async Job Scheduler Task by LogiScope Company

--- AsynchronousJobScheduler_By_Pavan/src/main/java/com/pavan/controller/JobController.java
+++ AsynchronousJobScheduler_By_Pavan/src/main/java/com/pavan/controller/JobController.java
@@ -25,6 +25,7 @@ public class JobController {
 		return ResponseEntity.ok("Job Triggered SuccessFully");
 		
 	}
+	
 	/*
 	 * @GetMapping("/trigger") public ResponseEntity<String> getMethod() { return
 	 * ResponseEntity.ok("Sucess"); }
