42b09b6e337783e22ceeb7d463458843b92d233b
PavanKumarB <bollupavankumar12@gmail.com>
2025-05-05 22:21:49 +0530

    Async Job Scheduler Task by LogiScope Company

--- AsynchronousJobScheduler_By_Pavan/src/main/java/com/pavan/service/JobService.java
+++ AsynchronousJobScheduler_By_Pavan/src/main/java/com/pavan/service/JobService.java
@@ -11,6 +11,14 @@ public class JobService {
 	
 	public void executeEmailTask(Map<String, String> params)
 	{
+		String recipient = params.getOrDefault("recipient", "PavanKumar@gmail.com");
+		String message = params.getOrDefault("message", "Mail Sending Task");
+		
+		try {
+			
+		} catch (InterruptedException e) {
+			// TODO: handle exception
+		}
 		
 	}
 	
