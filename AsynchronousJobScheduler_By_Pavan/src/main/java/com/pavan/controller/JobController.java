d24c8008af99d54d3c8a5b76dff137a142927d49
PavanKumarB <bollupavankumar12@gmail.com>
2025-05-05 22:42:20 +0530

    Async Job Scheduler Task by LogiScope Company

--- AsynchronousJobScheduler_By_Pavan/src/main/java/com/pavan/controller/JobController.java
+++ AsynchronousJobScheduler_By_Pavan/src/main/java/com/pavan/controller/JobController.java
@@ -1,10 +1,20 @@
 package com.pavan.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pavan.model.JobRequest;
 
 @RestController
 @RequestMapping("/api/jobs")
 public class JobController {
 
+	public ResponseEntity<String> triggerJob(JobRequest jobRequest)
+	{
+		return null;
+		
+	}
+	
+	
 }
