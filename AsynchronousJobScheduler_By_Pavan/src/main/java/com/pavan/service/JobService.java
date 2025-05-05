f6df2246bf5885d84e6c51ab5f6fadc11fe1b1f2
PavanKumarB <bollupavankumar12@gmail.com>
2025-05-05 22:18:04 +0530

    Async Job Scheduler Task by LogiScope Company

--- AsynchronousJobScheduler_By_Pavan/src/main/java/com/pavan/service/JobService.java
+++ AsynchronousJobScheduler_By_Pavan/src/main/java/com/pavan/service/JobService.java
@@ -1,9 +1,23 @@
 package com.pavan.service;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class JobService {
 	
+	
+	
+	public void executeEmailTask(Map<String, String> params)
+	{
+		
+	}
+	
+	
+	public void executeReportTask(Map<String, String> params)
+	{
+		
+	}
 
 }
