fe103f6ce9a8f38c16db32f89d80bde25ca07322
PavanKumarB <bollupavankumar12@gmail.com>
2025-05-05 22:37:23 +0530

    Async Job Scheduler Task by LogiScope Company

--- AsynchronousJobScheduler_By_Pavan/src/main/java/com/pavan/service/JobExecutorService.java
+++ AsynchronousJobScheduler_By_Pavan/src/main/java/com/pavan/service/JobExecutorService.java
@@ -1,8 +1,19 @@
 package com.pavan.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.pavan.model.JobRequest;
 
 @Service
 public class JobExecutorService {
+	
+	@Autowired
+	private JobService jobService;
+	
+	public void triggerJob(JobRequest jobRequest)
+	{
+		
+	}
 
 }
